80a0ad42af61018a82d29ff681f68554ef5da7f4
Diederik de Vries <d.de.vries@hr.nl>
2023-07-06 14:26:01 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -45,24 +45,9 @@ language = 'nl'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# latex_elements = {
-# 'extraclassoptions': 'openany,oneside'  
-# }
-
-
 latex_elements = {
-'fontpkg': r'''
-\setmainfont{DejaVu Serif}
-\setsansfont{DejaVu Sans}
-\setmonofont{DejaVu Sans Mono}
-''',
-'preamble': r'''
-\usepackage[titles]{tocloft}
-\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
-\setlength{\cftchapnumwidth}{0.75cm}
-\setlength{\cftsecindent}{\cftchapnumwidth}
-\setlength{\cftsecnumwidth}{1.25cm}
-''',
-    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
-    'printindex': r'\footnotesize\raggedright\printindex',
+'extraclassoptions': 'openany,oneside',
+'setmainfont': 'DejaVu Serif',
+'setsansfont': 'DejaVu Sans',
+'setmonofont': 'DejaVu Sans Mono'
 }
